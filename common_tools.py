b48b02c231ee760ca76eb97b90f14b9a1cd78aa9
Eduardo Seiti de Oliveira <eduseiti@gmail.com>
2023-09-20 08:14:39 -0300

    Additional experiments

--- common_tools.py
+++ common_tools.py
@@ -41,14 +41,83 @@ API_KEYS_FILE_2="../api_keys_20230612.json"
 # Queries LLM evaluation definitions
 #
 
+
 EVALUATION_SYSTEM_ROLE = {
     'role': "system", 
-    'content': "Você avalia se uma passagem de texto responde a uma pergunta, indicando uma pontuação de 0 à 3, onde 0 indica que a passagem não está relacionada com a pergunta; 1 indica que a passagem está no tema mas não responde a pergunta; 2 indica que a passagem responde parcialmente a pergunta ou responde mas inclui muita informação não relacionada; e 3 que a passagem responde a pergunta de forma clara e direta, sem conter informações não relacionadas. Sua deve ser JSON, com o primeiro campo \"Razão\", explicando seu raciocínio, e segundo campo \"Pontuação\""
+    'content': "Task description:\nEvaluate whether a passage of text answers a question, indicating a score from 0 to 3, where 0 = passage is inappropriate for the question; 1 = passage partially answers the question; 2 = passage answers the question but does not contain all the necessary information; and 3 = passage contains all the information to answer the question.\n\nEvaluation Steps:\n1. Read the question carefully: Understand what the question is asking. Identify the key points or information that the answer should contain.\n\n2. Read the passage: Read the passage thoroughly. Try to understand the main idea and the details provided in the passage.\n\n3. Compare the question and the passage: Compare the information in the passage with the requirements of the question. Identify if the passage contains the necessary information to answer the question.\n\n4. Score the passage: Based on your comparison, score the passage. If the passage is inappropriate for the question, score it 0. If the passage partially answers the question, score it 1. If the passage answers the question but does not contain all the necessary information, score it 2. If the passage contains all the information to answer the question, score it 3.\n\nExpected JSON output:\n{\"Razão\": \"<>\", \"Pontuação\":  <>}"
+}
+
+
+
+EVALUATION_SYSTEM_ROLE_REV_2_3 = {
+    'role': "system", 
+    'content': "Você avalia se uma passagem de texto responde a uma pergunta, atribuindo uma pontuação gradual de 0 a 3, onde 0 é passagem totalmente inadequada para a pergunta e 3 é passagem contém todas as informações para responder a pergunta”. Sua resposta deve ser JSON, com o primeiro campo \"Razão\", explicando seu raciocínio, e segundo campo \"Pontuação\""
+}
+
+
+
+EVALUATION_SYSTEM_ROLE_REV_2_2 = {
+    'role': "system", 
+    'content': "Você avalia se uma passagem de texto responde a uma pergunta, indicando uma pontuação de 0 à 3, onde 0 = passagem é inadequada para a pergunta; 1 = passagem responde parcialmente a pergunta; 2 = passagem responde a pergunta mas não contém todas as informações necessárias; e 3 = passagem contém todas as informações para responder a pergunta. Sua resposta deve ser JSON, com o primeiro campo \"Razão\", explicando seu raciocínio, e segundo campo \"Pontuação\""
+}
+
+
+EVALUATION_SYSTEM_ROLE_REV_2_1 = {
+    'role': "system", 
+    'content': "Você avalia se uma passagem de texto responde a uma pergunta, indicando uma pontuação de 0 à 3, onde 0 indica que a passagem não está relacionada com a pergunta; 1 indica que a passagem está no tema mas não responde a pergunta; 2 indica que a passagem responde parcialmente a pergunta ou responde mas inclui muita informação não relacionada; e 3 que a passagem responde a pergunta de forma clara e direta, sem conter informações não relacionadas. Sua resposta deve ser JSON, com o primeiro campo \"Razão\", explicando seu raciocínio, e segundo campo \"Pontuação\""
 }
 
 
 
 EVALUATION_FEW_SHOT_EXAMPLES=[
+    [
+        {
+            'role': "user",
+            'content': "Passagem: \"O cirurgião faz uma incisão no quadril, remove a articulação do quadril danificada e a substitui por uma articulação artificial que é uma liga metálica ou, em alguns casos, cerâmica. A cirurgia geralmente leva cerca de 60 a 90 minutos para ser concluída.\"\nPergunta: \"de que metal são feitas as próteses de quadril?\"", 
+        },
+        {  
+            'role': "assistant",
+            'content': "{\"Razão\":\"Passagem não responde completamente a pergunta, apenas indicando que a prótese pode ser de uma liga metálica, sem listar quais metais.\",\"Pontuação\":1}"
+        }
+    ],
+
+    [
+        {
+            'role': "user",
+            'content': "Passagem: \"Vanessa Redgrave nasceu em 30 de janeiro de 1937 em Londres. Ela fará falta, mas não será esquecida. Centenas de fãs imediatamente começaram a escrever suas mensagens de condolências na página do Facebook, expressando sua tristeza pela morte da talentosa atriz de 79 anos.\"\nPergunta: \"quantos anos tem vanessa redgrave?\"",
+        },
+        {
+            'role': "assistant",
+            'content': "{\"Razão\":\"A passagem responde a idade que Vanessa Redgrave tinha quando faleceu, e que ela nasceu em 1937. Com essas informações é possível determinar que ela morreu em 2016 e ter toda informação completa\",\"Pontuação\":3}"
+        }
+    ],
+
+    [
+        {
+            'role': "user",
+            'content': "Passagem: \"O Brasil possui muitas belezas naturais. Neste artigo vamos indicar os melhores lugares para passear no Brasil.\"\nPergunta: \"Onde passear no Brasil?\"",
+        },
+        {
+            'role': "assistant",
+            'content': "{\"Razão\":\"A passagem apenas indica que o Brasil tem muitas belezas naturais, mas não lista nenhum exemplo. Embora a passagem indique que artigo vai falar sobre lugares para passear no Brasil, o trecho apresentado não lista nenhum lugar específico para passear no Brasil\",\"Pontuação\":0}"
+        }
+    ],
+
+    [
+        {
+            'role': "user",
+            'content': "Passagem: \"conveniente no bairro mais moderno da cidade, o Radisson Blu Belo Horizonte, Savassi é um dos hotéis cinco estrelas mais desejados do Sudeste do Brasil. Passe o dia comprando produtos de marcas famosas no Pátio Savassi ou visitando a Pampulha, um Patrimônio Mundial da UNESCO maravilhosamente preservado, antes de voltar para um dos nossos quartos de hotel modernos. Quando estiver pronto para se aventurar novamente, caminhe pelo movimentado Mercado Central de Belo Horizonte para comprar souvenires ou frutas frescas e provar lanches tradicionais brasileiros, como o pão de queijo. Comece seu dia com o pé direito saboreando um café da manhã de cortesia na Pizzaria Olegário e volte mais tarde para um delicioso almoço ou jantar. Viajando a trabalho? Com um centro de negócios, estacionamento amplo e quatro espaços de\"\nPergunta: \"Onde passear no Brasil?\"",
+        },
+        {
+            'role': "assistant",
+            'content': "{\"Razão\":\"A passagem responde à pergunta indiretamente incluindo informação desnecessária, indicando que na região do hotel em Belo Horizonte fica um Patrimônio Mundial da UNESCO, que pode ser uma boa sugestão de passeio no Brasil\",\"Pontuação\":2}"
+        }
+    ], 
+]
+
+
+
+EVALUATION_FEW_SHOT_EXAMPLES_REV_2_1=[
     [
         {
             'role': "user",
@@ -93,9 +162,6 @@ EVALUATION_FEW_SHOT_EXAMPLES=[
         }
     ], 
 ]
-
-
-
 
 
 
@@ -185,7 +251,7 @@ EVALUATION_FEW_SHOT_EXAMPLE_FORMAT="Exemplo {}:\n{}"
 EVALUATION_SINGLE_FEW_SHOT_PROMPT_INITIAL_FORMAT="{} Siga os exemplos abaixo."
 
 EVALUATION_QUERY_PASSAGE_FORMAT="Passagem: \"{}\"\nPergunta: \"{}\""
-EVALUATION_OPENAI_RESPONSE_REGEX="[\n\r]*\{\s*[\"\']Razão[\"\']\s*:\s*\"(.+)\"\s*,\s*[\"\']Pontuação[\"\']\s*:\s*([0-9]+)\s*\}[\.\n\r]*"
+EVALUATION_OPENAI_RESPONSE_REGEX="[\n\r]*\{\s*[\"\']Razão[\"\']\s*:\s*\"(.+)\"\s*,\s*[\"\']Pontuação[\"\']\s*:\s*([0-9\.]+)\s*\}[\.\n\r]*"
 
 EVALUATION_MAX_TOKENS_RESPONSE=500
 
@@ -356,7 +422,7 @@ def execute_LLM_passage_relevance_evaluation(which_query,
         m = re.match(EVALUATION_OPENAI_RESPONSE_REGEX, response_text)
 
         if len(m.groups()) == 2:
-            score = int(m.group(2))
+            score = int(round(float(m.group(2))))
             reasoning = m.group(1)
         else:
             score = None
@@ -502,7 +568,9 @@ def LLM_query_passage_evaluation(query_passages_df,
                 print(e)
 
                 if number_of_completions > 1:
-                    time.sleep(45)
+                    time.sleep(60)
+                else:
+                    time.sleep(30)
 
                 relevance_results = execute_LLM_passage_relevance_evaluation(row['query'], 
                                                                              row['passage'], 
